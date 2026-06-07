fe5e4194f830b1d51bd92166017efa1f3743372d
Peter Knall <peterknall@gmail.com>
2026-06-07 11:30:05 -0400

    Added pandas dependency note

--- zcad.py
+++ zcad.py
@@ -1,16 +1,40 @@
 # This is a sample Python script.
+from numpy.testing.print_coercion_tables import print_new_cast_table
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from pk.utils.postgres.Tools import Tools as PSQL_Tools
+import pandas as pd
 
+SITE_TABLE_NAME = "site_metadata"
+ROOM_TABLE_NAME = "room_metadata"
+TREND_TABLE_NAME = "trend_metadata"
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+TARGET_TREND_DOMAIN_NAMES = ('desiredCoolingTemp', 'desiredHeatingTemp', 'currentTemp')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    psql_tools = PSQL_Tools()
+    # Get a list of Sites.  Until more sites are populated only Canal Park Stadium is used.
+    site_ref = "20a474cf-1eb8-405b-af54-6a5d059dafcf"   # Canal Park Stadium
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Get a list of rooms from that site ###############################################################################
+    room_query_string = f"SELECT * FROM {ROOM_TABLE_NAME} WHERE siteref = '{site_ref}'"
+    # print(room_query_string)
+    room_results = psql_tools.execute_read_only_query(room_query_string)
+    # print(room_results)
+    rooms_df = pd.DataFrame(room_results)
+    # print(rooms_df)
+
+    # Get a list of trends from that room ##############################################################################
+    for room_id in rooms_df[0]:
+        # print(room_id)
+        trend_query_string = f"SELECT * FROM {TREND_TABLE_NAME} WHERE roomref = '{room_id}' and domainname in {TARGET_TREND_DOMAIN_NAMES};"
+        print(trend_query_string)
+        trend_results = psql_tools.execute_read_only_query(trend_query_string)
+        trend_df = pd.DataFrame(trend_results)
+        for trend_id in trend_df[0]:
+            print(trend_id)
+
+    # Get a trend values from those trends for some period #############################################################
+
+
+        break   # run once
